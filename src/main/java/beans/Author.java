fd8c99827dc383389acf2673aea962617a0b6a33
ReshetnevMihail <48208585+ReshetnevMihail@users.noreply.github.com>
2022-07-07 17:19:05 +0300

    Add files via upload

---
+++ src/main/java/beans/Author.java
@@ -0,0 +1,93 @@
+package beans;
+
+import java.io.Serializable;
+import java.util.Date;
+
+
+public class Author implements Serializable{
+    private int id;
+    private String name;
+    private Date dateBirth;
+
+    public Author(){
+
+    }
+    public Author(String name) {
+        this.name = name;
+    }
+
+    public Date getDateBirth() {
+        return dateBirth;
+    }
+
+    public void setDateBirth(Date dateBirth) {
+        this.dateBirth = dateBirth;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "Author[" +
+                "name='" + name + '\'' +
+                ']';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Author author = (Author) o;
+
+        if (id != author.id) return false;
+        return name.equals(author.name);
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id;
+        result = 31 * result + name.hashCode();
+        return result;
+    }
+
+//    private String name;
+//
+//    public Author(){
+//
+//    }
+//
+//    public Author(String name) {
+//        this.name = name;
+//    }
+//
+//    public String getName() {
+//        return name;
+//    }
+//
+//    public void setName(String name) {
+//        this.name = name;
+//    }
+//
+//    @Override
+//    public String toString() {
+//        return "Author{" +
+//                "name='" + name + '\'' +
+//                '}';
+//    }
+}
